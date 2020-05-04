1c8df8fcfdf76ae788289721c5111f2562fbe47b
Dave <dave.cadelina@outlook.com>
2020-05-04 17:44:04 +1000

    Added Javscript code

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-const date = new Date();
-console.log(date)
-console.log(date.getHours())
-console.log(date.getMinutes())
-console.log(date.getSeconds())
+const hours_span = document.getElementById('hours');
+const minutes_span = document.getElementById('minutes');
+const seconds_span = document.getElementById('seconds');
+
+function refreshClock(){
+    let refresh=1000; // Refresh rate in milli seconds
+    mytime=setTimeout('clock();',refresh)
+}
+
+function clock() {
+    let current_time = new Date();
+
+    if (current_time.getHours() < 10) {
+        hours_span.innerHTML = `0${current_time.getHours()}`;
+    } else {
+        hours_span.innerHTML = current_time.getHours();
+    }
+
+
+    if (current_time.getMinutes() < 10) {
+        minutes_span.innerHTML = `0${current_time.getMinutes()}`;
+    } else {
+        minutes_span.innerHTML = current_time.getMinutes();
+    }
+        
+    
+    if (current_time.getSeconds() < 10) {
+        seconds_span.innerHTML = `0${current_time.getSeconds()}`;
+    } else {
+        seconds_span.innerHTML = current_time.getSeconds();
+    }
+    refreshClock();
+}
+
+clock();
